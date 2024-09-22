3bcd8beef9c31bd95c2d13aa5947dc84f2345126
Alun Jones <alunacjones@gmail.com>
2024-09-22 13:19:27 +0100

    ready to build

--- LSL.AppVeyor.Helpers.Tests/AppVeyorFixingStringExtensionsTests.cs
+++ LSL.AppVeyor.Helpers.Tests/AppVeyorFixingStringExtensionsTests.cs
@@ -6,6 +6,7 @@ namespace LSL.AppVeyor.Helpers.Tests;
 public class AppVeyorFixingStringExtensionsTests
 {
     [TestCase("a test", "a test")]
+    [TestCase("a test\r\nand another line", "a test\r\nand another line")]
     [TestCase("a test\nand another line", "a test\r\nand another line")]
     public void GivenAStringToFix_ThenItShouldProduceTheExpectedResult(string input, string expectedOutput)
     {
